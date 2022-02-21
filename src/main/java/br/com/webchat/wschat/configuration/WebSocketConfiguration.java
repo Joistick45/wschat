38db458fa32e580a21c0c44448b0343ef7574418
Joi Macedo <joi.macedo@hotmail.com>
2022-02-21 09:09:36 -0300

    wschat backend

---
+++ src/main/java/br/com/webchat/wschat/configuration/WebSocketConfiguration.java
@@ -0,0 +1,31 @@
+package br.com.webchat.wschat.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+import br.com.webchat.wschat.handler.ChatWebSocketHandler;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfiguration implements WebSocketConfigurer{
+
+	private final static String CHAT_ENDPOINT = "/chat";
+	
+	
+	@Override
+	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+		registry.addHandler(getChatWebSocketHandler(), CHAT_ENDPOINT )
+		.setAllowedOrigins("*");
+		
+	}
+	
+	@Bean
+	public WebSocketHandler getChatWebSocketHandler() {
+		return new ChatWebSocketHandler();
+	}
+
+}
---
+++ src/test/java/br/com/webchatenroloes/wschat/WsChatApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.webchatenroloes.wschat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
